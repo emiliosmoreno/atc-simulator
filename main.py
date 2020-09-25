ec69f19367ee30a74c6ca25a1aaadef6e3b12d9d
emiliosmoreno <git@emiliosmoreno.com>
2020-09-25 23:48:19 +0200

    <develop> Creación de la rama develop. Inclusión de comentarios

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import sys, pygame
 from pygame.locals import *
 from test._test_multiprocessing import DELTA
 
+# Clase principal del juego
 class ATCsimulator:
 
+    # Método de inicialización 
     def __init__(self):
         
         # Constantes
@@ -30,7 +32,8 @@ class ATCsimulator:
         pygame.display.set_caption("ATC Simulator by @emiliosmoreno")
         pintar_icono(self)
         self.en_pausa=False 
-          
+    
+    # Ejecución del juego      
     def run(self):
         
         while True:
@@ -52,15 +55,18 @@ class ATCsimulator:
             pygame.display.update()                
         return 0 
 
+# Método encargado de configurar el icono del juego
 def pintar_icono(self):
     self.icono = pygame.image.load('img/icon.png')
     pygame.display.set_icon(self.icono)
 
+# Método encargado de pintar el espacio aéreo
 def pintar_espacio_aereo(self):
     self.espacio_aereo = pygame.Surface(self.SIZE_ESPACIO_AEREO)  
     pygame.draw.rect(self.espacio_aereo, self.CL_BLACK, self.espacio_aereo.get_rect())
     self.escena.blit(self.espacio_aereo, (50, 50))
 
+# Método encargado de gestionar el aviso de pausa en el juego
 def pintar_pausa_juego(self):
     
         if (self.en_pausa): 
@@ -68,7 +74,8 @@ def pintar_pausa_juego(self):
                                          -1,
                                          (255, 255, 255)),
                             (self.WIDTH/2 - 50, 50))
-           
+
+# Método que gestiona los eventos del juego           
 def gestion_eventos(self):
     for evento in pygame.event.get():
         if evento.type == pygame.KEYDOWN:
@@ -76,7 +83,8 @@ def gestion_eventos(self):
                 self.en_pausa = not self.en_pausa
         if evento.type == QUIT:
             sys.exit(0)    
-              
+
+# Llamada principal          
 if __name__ == "__main__":
     pygame.init()
     ATCsimulator().run()
