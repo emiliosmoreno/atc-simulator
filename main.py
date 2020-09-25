82bb1665f6f989c1767f7a1252ca3bf237b14540
emiliosmoreno <git@emiliosmoreno.com>
2020-09-25 21:27:31 +0200

    <iteración 2> Generación del espacio aéreo

--- main.py
+++ main.py
@@ -3,20 +3,53 @@
 
 import sys, pygame
 from pygame.locals import *
+from test._test_multiprocessing import DELTA
 
 # Constantes
 WIDTH = 800
 HEIGHT = 600
+DELTA = 50
+VELOCIDAD=60
+
+SIZE_WINDOW = (WIDTH, HEIGHT)
+SIZE_ESPACIO_AEREO = (WIDTH-DELTA*2, HEIGHT-DELTA*2)
+
+CL_WHITE=(255,255,255)
+CL_BLUE=(0,0,255)
+
+
 
 def Main():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Pruebas Pygame")
+    clock = pygame.time.Clock()
+    escena = pygame.display.set_mode(SIZE_WINDOW)
+    pygame.display.set_caption("ATC Simulator by @emiliosmoreno")
+    pintar_icono()
+   
+    
     while True:
-        for eventos in pygame.event.get():
-            if eventos.type == QUIT:
-                sys.exit(0)
+        clock.tick(VELOCIDAD)
+        
+        pintar_espacio_aereo(escena);
+        
+        gestion_eventos()
+        
+        pygame.display.update()                
     return 0 
-  
+
+def pintar_icono():
+    icono = pygame.image.load('img/icon.png')
+    pygame.display.set_icon(icono)
+
+def pintar_espacio_aereo(escena):
+    espacio_aereo = pygame.Surface(SIZE_ESPACIO_AEREO)  
+    pygame.draw.rect(espacio_aereo, CL_WHITE, espacio_aereo.get_rect())
+    escena.blit(espacio_aereo, (50, 50))
+    
+def gestion_eventos():
+    for eventos in pygame.event.get():
+        if eventos.type == QUIT:
+            sys.exit(0)    
+              
 if __name__ == "__main__":
     pygame.init()
     Main()
