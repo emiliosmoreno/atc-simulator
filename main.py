a6e8dcf913efcb73d9d7c30e3c36431dd0841361
emiliosmoreno <git@emiliosmoreno.com>
2020-09-26 20:46:23 +0200

    <feature-generacion> Generación de los aviones

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import sys, pygame, math
+import sys, pygame, math, random
 from pygame.locals import *
 
 # Constantes
@@ -22,39 +22,64 @@ class Constantes:
     CL_BLUE=(0,0,255)
     CL_BLACK=(0,0,0)
     CL_WHITE=(255,255,255)
+    AIRLINES=["IB","AFL","AFR","VLG"]
     
 class Avion:
     def __init__(self, escena, x, y, velocidad, altura):
-        self.velocidad = velocidad
+        self.id=Constantes.AIRLINES[random.randint(1,3)]+str(random.randint(0,99))
         self.posx=x
         self.posy=y
-        self.altura=altura
+        self.posz=altura
         self.escena=escena
+        self.velocidad = velocidad / 100
         self.direccion=180 #Valores permitidos: 1-360 (grados)s
         self.font = pygame.font.SysFont("Arial", 12)
-        self.id=100
+        
         
     def avanzar(self):
        
+        desplazamiento_x=math.cos(math.radians(self.direccion))
+        desplazamiento_y=math.sin(math.radians(self.direccion))
         if (self.posx < 0):
-            self.posx = -self.posx + math.cos(math.radians(self.direccion))
+            self.posx = -self.posx + desplazamiento_x-self.velocidad
         
         if (self.posx > 0):
-            self.posx = self.posx + math.cos(math.radians(self.direccion))
+            self.posx = self.posx + desplazamiento_x -self.velocidad
         
         self.posy = self.posy + math.sin(math.radians(self.direccion))
-        print(str(self.posx)+","+str(self.posy))
+        #print(str(self.posx)+","+str(self.posy)+" - dx="+str(desplazamiento_x)+", dy="+str(desplazamiento_y)+", v="+str(self.velocidad)+", mv="+str(self.velocidad/100))
+        #self.velocidad = self.velocidad+0.01
         
     def pintar(self):
-        self.rect= pygame.Rect(self.posx, self.posy, 5, 5)
-        pygame.draw.rect(self.escena, Constantes.CL_WHITE, self.rect)
+        self.airplane=[]
         
+        #print(self.id+" - v="+str(self.velocidad))
+        traza=self.velocidad
+        tamanyo=3
+        distancia=5
+        self.airplane.append(pygame.Rect(self.posx, self.posy, tamanyo, tamanyo))
+        
+        
+
+        #while (traza>0):
+        #    self.airplane.append(pygame.Rect(self.posx+distancia, self.posy+distancia, tamanyo, tamanyo))
+        #    tamanyo=tamanyo-0.5
+        #    distancia=distancia+5
+        #    traza=traza-0.5
+        
+        for traza_avion in self.airplane:  
+            pygame.draw.rect(self.escena, Constantes.CL_WHITE, traza_avion) 
+            
     def pintar_id(self):
         
-        if (self.id<100):
+        if (len(self.id)<2):
             self.desp=0
-        if (self.id>=100 and self.id<1000):
-            self.desp=1    
+        else:
+            if (len(self.id)>2 and len(self.id)<5):
+                self.desp=1
+            else:
+                self.desp=2
+                 
         self.escena.blit(self.font.render(str(self.id),
                                          -1,
                                          (255, 255, 255)),
@@ -66,10 +91,6 @@ class ATCsimulator:
     # Método de inicialización 
     def __init__(self):
         
-      
-        
-        
-        
         self.clock = pygame.time.Clock()
         pygame.font.init()
         self.font = pygame.font.SysFont("Arial", 50)
@@ -80,8 +101,12 @@ class ATCsimulator:
         self.en_pausa=False 
     
         self.aviones =[]
+        self.num_aviones=0
         
-        new_avion = Avion(self.escena, 900,300, 0.1,2000)
+        new_avion = Avion(self.escena, 850,300, 100,2000)
+        self.aviones.append(new_avion)
+        
+        new_avion = Avion(self.escena, 850,350, 150,2000)
         self.aviones.append(new_avion)
         
     # Ejecución del juego      
@@ -105,7 +130,8 @@ class ATCsimulator:
             
             pintar_pausa_juego(self)
                     
-            pygame.display.update()                
+            pygame.display.update()
+                            
         return 0 
 
 # Método encargado de configurar el icono del juego
@@ -122,6 +148,7 @@ def pintar_espacio_aereo(self):
 
 # Método encargado de pintar los aviones en el espacio aéreo
 def pintar_aviones(self):
+    print("\n")
     for avion in self.aviones:
         avion.avanzar()
         avion.pintar()
@@ -131,13 +158,12 @@ def pintar_aviones(self):
 def pintar_pausa_juego(self):
     
         if (self.en_pausa): 
-            self.escena.blit(self.font.render("PAUSA",
-                                         -1,
-                                         (255, 255, 255)),
-                            (Constantes.WIDTH/2 - 50, 50))
+            self.escena.blit(self.font.render("PAUSA",-1,Constantes.CL_WHITE),
+                             (Constantes.WIDTH/2 - 50, 50))
 
 # Método que gestiona los eventos del juego           
 def gestion_eventos(self):
+    
     for evento in pygame.event.get():
         if evento.type == pygame.KEYDOWN:
             if (evento.key == K_p):
